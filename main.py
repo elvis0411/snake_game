e99f2d09c8f2ad8c975ab5b9de8ae753e072133b
Elvis <91163415+elvis0411@users.noreply.github.com>
2021-12-30 00:54:21 -0800

    add scoreboard function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from turtle import Screen, Turtle
+from turtle import Screen, Turtle, position
 import time
 
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
    
 level = 10
@@ -18,12 +20,21 @@ screen.onkey(fun = snake.turn_north,key ="w")
 screen.onkey(fun = snake.turn_west,key ="a")
 screen.onkey(fun = snake.turn_south, key = "s")
 screen.onkey(fun = snake.turn_east,key ="d")    
+screen.listen()
 
-
+food = Food()
+scoreboard = Scoreboard()
 while snake.is_game_on():
-    screen.update() 
-    screen.listen() 
+    screen.update()      
     snake.move()       
+    if snake.snake_head.distance(food.food.position()) < 15:
+        print("nom nom nom")
+        #old food gone
+        food.refresh()
+        #count for the score
+        scoreboard.add_score()
+        #add new segment
+        #new food in new location
     time.sleep(0.1)
 
     
